2dbb4ccd9ddc9ef61a057a128dd11f23b106ecd5
Gpuclla <48296122+Gpuclla@users.noreply.github.com>
2021-11-18 12:47:35 -0500

    Initial commit

---
+++ app/src/main/java/com/example/actividades/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.actividades;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    public static final long SPLASH_SCREEN_DELAY = 4000;
+    MediaPlayer mp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        mp=MediaPlayer.create(SplashActivity.this,R.raw.introtecla);
+        mp.start();
+        TimerTask time= new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent().setClass(SplashActivity.this, SplashActivity2.class);
+                startActivity(intent);
+                finish();
+
+            }
+        };
+        Timer timer=new Timer();
+        timer.schedule(time,SPLASH_SCREEN_DELAY);
+    }
+}
